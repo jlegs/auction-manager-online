27a4fbcc4749db51209688d5878d3b59bbb9acf7
Josh B <josh.s.brown@hotmail.com>
2014-06-26 16:59:12 -0400

    updating docstrings and adding a full_deploy to fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,7 @@ from fabric.api import local, run, env
 env.hosts = ['ec2-54-82-112-202.compute-1.amazonaws.com']
 env.user = 'wildlife'
 env.forward_agent = True
+
 def test():
     local('./manage.py test')
 
@@ -14,17 +15,33 @@ def remote_in():
 
 def gunicorn(arg):
     '''
-    gunicorn command. inserts any arguments you send with it to the supervisorctl command.
+    gunicorn command. inserts any arguments you send with it to the supervisorctl command. run this after ""fab deploy_code""
     e.g.: fab gunicorn:restart == supervisorctl restart wildlife
     '''
     run('cd website/auction-manager-online && source ../../.virtualenvs/wildlife/bin/activate && supervisorctl %s wildlife' % arg)
 
 
 def deploy_code():
+    '''
+    Pulls the latest version of the codebase to the server
+    '''
     run('cd website/auction-manager-online && git pull')
 
 def install_dependencies():
+    '''
+    runs pip install -r requirements.txt to install the dependencies.
+    '''
     run('cd website/auction-manager-online && source ../../.virtualenvs/wildlife/bin/activate && pip install -r requirements.txt')
 
 
+
+def full_deploy():
+    '''
+    Gets latest code on the server, installs dependencies and restarts gunicorn for the changes to take effect.
+    '''
+    run('cd website/auction-manager-online && git pull')
+
+    run('cd website/auction-manager-online && source ../../.virtualenvs/wildlife/bin/activate && pip install -r requirements.txt')
+
+    run('cd website/auction-manager-online && source ../../.virtualenvs/wildlife/bin/activate && supervisorctl restart wildlife')
 

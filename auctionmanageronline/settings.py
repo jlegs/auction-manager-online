0eb035288890d7ca5be19ee22684204bc917395e
Josh B <josh.s.brown@hotmail.com>
2014-06-27 12:51:39 -0400

    setting up for prod environment

--- auctionmanageronline/settings.py
+++ auctionmanageronline/settings.py
@@ -16,13 +16,22 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%^1cwo3=z1!si0g8d7rc)k+xr83z5gm6+9@4($t@e(n2@_3_a9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+if not DEBUG:
+    with open('/home/wildlife/secret_key.txt') as f:
+        SECRET_KEY = f.read().strip()
+else:
+    SECRET_KEY = 'j4io2joidj90*)*#*#)()uf23j90)*&3}}{|%($^oijun'
+
+
 
 ALLOWED_HOSTS = []
 

6325401621ce8c10e051d18745b73154faf5ba82
Josh B <josh.s.brown@hotmail.com>
2014-06-29 15:30:57 -0400

    adding to admin site

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from manager.models.attendee import Attendee
+from manager.models.auction_item import AuctionItem
+from manager.models.invoice import Invoice
 
 # Register your models here.
+
+admin.register(Attendee)
+admin.register(AuctionItem)
+admin.register(Invoice)
+
